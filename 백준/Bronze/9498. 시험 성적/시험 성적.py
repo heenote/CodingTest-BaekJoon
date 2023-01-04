1da9e9c0d2307024ee4ee0245b9b50d72533875a
JoSnagHee <99184080+heenote@users.noreply.github.com>
2023-01-04 09:09:48 +0900

    [Bronze V] Title: 시험 성적, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,11 @@
+x= int(input())
+if 90 <= x <= 100:
+    print('A')
+elif 80<= x < 90:
+    print('B')
+elif 70 <= x < 80:
+    print('C')
+elif 60<= x < 70:
+    print('D') 
+elif x < 60:
+    print('F')
