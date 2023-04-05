e0ca9d18b489b4ba5e8523b88a15026c18154274
JoSnagHee <99184080+heenote@users.noreply.github.com>
2023-04-05 16:39:44 +0900

    [Bronze II] Title: 소수 찾기, Time: 40 ms, Memory: 33376 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -1,12 +1,13 @@
+import math
 X = int(input())
 Y = map(int,(input().split()))            
 cnt = 0  
 for i in Y:
     check = 0
     if i >1:
-        for j in range(2, i):
+        for j in range(2, int(math.sqrt(i)+1)):
             if i % j == 0:
               check += 1
         if check == 0:
             cnt+=1    
-print(cnt)   
+print(cnt)  
